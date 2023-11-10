86cf07ec5d2d845d5feb8f4d12c44a1fc04975c2
nakevin <nakevin004@naver.com>
2023-11-10 12:17:53 +0900

    stack,queue

---
+++ BEAN4222/stacklink.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h> 
+typedef struct Node 
+{
+	int data;
+	struct Node *next;
+}Node; 
+typedef struct Stack 
+{
+	Node *front;
+	Node *rear; 
+	int count;
+}Stack; 
+void initStack(Stack *stack)
+{
+	stack->front = stack->rear = NULL; 
+	stack->count = 0;   
+}
+ 
+int isEmpty(Stack *stack)
+{
+	return stack->count == 0;   
+}
+ 
+void enstack(Stack *stack, int data)
+{
+	Node *newNode = (Node *)malloc(sizeof(Node)); 
+	newNode->data = data;
+	newNode->next = NULL;
+ 
+	if (isEmpty(stack)){
+		stack->front = newNode;       
+	}
+	else{
+		stack->rear->next = newNode;
+	}
+	stack->rear = newNode;   
+	stack->count++;
+}
+ 
+int destack(Stack *stack)
+{
+	int data;
+	Node *ptr;
+	if (isEmpty(stack)){
+		printf("Error : Stack is empty!\n");
+		return 0;
+	}
+	ptr = stack->rear; 
+	data = ptr->data;    
+	stack->rear = ptr->front;  
+	free(ptr);    
+	stack->count--;  
+	return data;
+}
+ 
+int main(void)
+{
+	int i;
+	Stack stack;
+	initStack(&stack);
+enstack(&stack,10);
+enstack(&stack,20);
+enstack(&stack,30);
+printf("%d\n",destack(&stack));
+printf("%d\n",destack(&stack));
+printf("%d\n",destack(&stack));
+ 
+	return 0;
+}
